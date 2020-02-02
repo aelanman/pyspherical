90d4cd6925f067087841488065b82d9ca4a3d9d8
Adam Lanman <adam_lanman@brown.edu>
2020-02-02 15:16:24 -0500

    add function definitions for forward transform steps

---
+++ transforms.py
@@ -0,0 +1,174 @@
+
+import numpy as np
+from scipy.special import sph_harm
+from wigner import dMat
+
+from utils import resize_axis
+
+
+
+# Structure:
+#   Functions for separate transform methods/parts of transforms.
+#   Wrapper that lets you select which method to use.
+#   Function to provide appropriate sampling if transforming a function.
+
+
+
+def unravel_lm(l, m):
+    return l*(l + 1) +  m
+
+def ravel_lm(ind):
+    l = int(np.floor(np.sqrt(ind)))
+    m = ind - l * (l + 1)
+
+    return l, m
+
+
+def _theta_fft(Gm_th, thetas, lmax, lmin=0, s=0):
+    """
+    Do transform over theta, using the approach in [SSHT authors].
+    """
+
+    Nt = thetas.size
+    dth = thetas[1] - thetas[0]
+
+    # Apply periodic extension in theta.
+    Gm_th = np.pad(Gm_th, [(0,0), (0, Nt-1)],  mode='constant')
+    em = np.fft.ifftshift(np.arange(-(lmax-1), lmax))
+    Gm_th[:, Nt:] = ((-1.0)**(s + em) * Gm_th[:, 2*Nt-2 - np.arange(Nt, 2*Nt - 1)].T).T
+    
+    Fmm = np.fft.fft(Gm_th, axis=1) / (2* Nt - 1)
+
+    if thetas[0] > 0:
+#       Fmm = (Fmm.T *  np.exp(-1j * em * dth)).T
+        Fmm = (Fmm.T * np.exp(-1j * em * np.pi/(2*Nt -1))).T     # TODO -- Why is this correct?
+        # TODO Careful with this. If Fmm's axes are the same size the behavior is inconsistent.
+
+    ## Truncate/zero-pad the m' axis, then convolve with weights over m'
+    padFmm = resize_axis(Fmm, (2*lmax - 1), axis=1, mode='zero')
+    padFmm = np.fft.fftshift(padFmm, axes=1)
+    
+    ## Get the weights:
+    def weight(em):
+        if np.abs(em) == 1:
+            return np.sign(em) * np.pi * 1j / 2.
+        if em % 2 == 1:
+            return 0
+        return 2/(1 - em**2)
+    
+    em_ext = range(-2*(lmax - 1), 2 * (lmax - 1)+1)
+    ite = (weight(mm) for mm in em_ext)
+    weights = np.fromiter(ite, dtype=complex)[::-1]
+    
+    def do_conv(a):
+        # The "valid" option only works with these Fourier-transformed
+        # quantities if they have been FFT-shifted, such that the 0 mode
+        # is in the middle of the array.
+        return np.convolve(a, weights, mode='valid')
+    
+    Gmm = np.apply_along_axis(do_conv, 1, padFmm) * 2*np.pi
+    
+    # Unshift the m' axis
+    Gmm = np.fft.ifftshift(Gmm, axes=1)
+
+    return Gmm
+
+def _dmm_to_flm(dmm, lmax, spin):
+    ## TODO -- numba accelerate this.
+
+    flm = np.zeros((1+lmax)**2, dtype=complex)
+    wig_d = dMat(lmax)
+
+    for el in range(lmax + 1):
+        prefac = np.sqrt((2*el + 1)/(4*np.pi))
+        for m in range(-el, el+1):
+            ind = unravel_lm(el, m)
+            prefac2 = (-1)**(el+m) * (1j)**(m+s)
+            for mp in range(-el, el+1):
+                flm[ind] += prefac * prefac2 * wig_d[el, mp, m] * wig_d[el, -s, mp] * dmm[m, mp]
+
+    return flm
+        
+
+def _do_transform_on_grid(dat, phis, thetas, lmax, lmin=0, ph_ax=0, th_ax=1, spin=0):
+    """
+    Do forward transform, assuming a regular grid in both theta and phi.
+    """
+
+    if th_ax == 0:
+        # Underlying functions expect the theta and phi axes to be 1 and 0, resp.
+        dat = dat.T
+
+    Nf, Nt = dat.shape
+
+    # Transform phi to m and pad/truncate.
+    dm_th = np.fft.fft(dat, axis=0) / Nf
+    dm_th = resize_axis(dm_th, (2*lmax - 1), mode='zero', axis=0)
+
+    # Transform theta to m'.
+    # If evenly-spaced in theta, can use an FFT.
+
+    dth = np.diff(thetas)
+    if np.allclose(dth, dth[0]):
+        dmm = _theta_fft(dm_th, thetas, lmax, lmin, spin)
+    else:
+        raise NotImplementedError("Non-equispaced latitudes are not yet supported.")
+    
+    flm = _dmm_to_flm(dmm, lmax, spin) 
+
+    return flm 
+
+
+def _do_transform_nongrid(*args, **kwargs):
+    raise NotImplementedError("Non-grid sampling is not yet supported.")
+
+
+def forward_transform(dat, phis, thetas, lmax, lmin=0, spin=0):
+    """
+    
+    """
+
+    grid = True
+    if dat.shape == (phis.size, thetas.size):
+        fax, tax = 0, 1
+    elif dat.shape == (thetas.size, phis.size):
+        fax, tax = 1, 0
+    elif dat.shape == thetas.shape == phis.shape:
+        grid = False
+    else:
+        raise ValueError("Data shapes inconsistent:"
+                         "\n\tdat.shape = {}"
+                         "\n\tphis.shape = {}"
+                         "\n\tthetas.shape = {}".format(str(dat.shape), str(phis.shape), str(thetas.shape)))
+
+    if grid:
+        return _do_transform_on_grid(dat, phis, thetas, lmax, lmin, fax, tax, spin)
+
+    else:
+        return _do_transform_nongrid(dat, phis, thetas, lmax, lmin, spin)
+
+ 
+
+
+if __name__ == '__main__':
+    
+    s = 0   # Spin
+    lmax = 10   # Maximum el
+    Nt = 701   # Number of samples in theta (must be odd)
+    Nf = 701 # Samples in phi
+#    Nf = 2*lmax-1
+#    Nt = lmax
+    
+    # Define samples
+    dth = np.pi/(2*Nt-1)
+    theta = np.linspace(dth, np.pi, Nt, endpoint=True)
+    phi = np.linspace(0, 2*np.pi, Nf, endpoint=False)
+    
+    # Data, shape (Nf , Nt)
+    gtheta, gphi = np.meshgrid(theta, phi)
+    dat = 10 * sph_harm(3,5, gphi, gtheta)
+    dat += 11 * sph_harm(4,8, gphi, gtheta)
+
+    #res = phi_fft(dat, phi, theta)
+    flm = forward_transform(dat, phi, theta, lmax)
+    import IPython; IPython.embed()
